a3f7e2a32e1927d60b795661f5b60177894b2683
Majid Sayed <sydmajid04@gmail.com>
2022-03-30 19:21:17 +0530

    bag updated

--- JavaScript/bag.js
+++ JavaScript/bag.js
@@ -5,7 +5,7 @@ function myFunction(){
 }
 
 var bagItems = JSON.parse(localStorage.getItem("bagData"))
-bagItems.map(function(elem){
+bagItems.map(function(elem,index,arr){
     // this is a div box
     var mainBox = document.createElement("div")
     mainBox.setAttribute("id", "mainBox")
@@ -30,9 +30,24 @@ bagItems.map(function(elem){
     var btn = document.createElement("button")
     btn.textContent = "REMOVE ITEM"
     btn.setAttribute("class", "removeBtn")
+    btn.addEventListener("click", function(){
+        removeProduct(elem,index,arr);
+    });
 
     imgBox.append(img)
     prodBox.append(price,name,btn)
     mainBox.append(imgBox,prodBox)
     document.querySelector("#bagProducts").append(mainBox)
 })
+
+function removeProduct(elem,index){
+    bagItems.splice(index, 1)
+    localStorage.setItem("bagData", JSON.stringify(bagItems))
+    window.location.reload()
+}
+
+var totalPrice = bagItems.reduce(function(sum,elem,index,arr){
+    return sum + Number(elem.price);
+}, 0)
+
+document.querySelector("#displayPrice").textContent = totalPrice
