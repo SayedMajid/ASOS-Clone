28429b773b57acda5c17ab0df49477c1dbb2880b
Majid Sayed <sydmajid04@gmail.com>
2022-03-31 15:24:00 +0530

    progress

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -14,5 +14,46 @@ var bagItems = JSON.parse(localStorage.getItem("bagData"))
 console.log(bagItems)
 
 bagItems.map(function (elem){
-    
-})
+    var mainBox = document.createElement("div")
+    mainBox.setAttribute("id", "mainBox")
+
+    var imgBox = document.createElement("div")
+    imgBox.setAttribute("id", "imgBox")
+
+    var prodBox = document.createElement("div")
+
+    var img = document.createElement("img")
+    img.src = elem.imgUrl;
+    img.setAttribute("class", "prodImage")
+
+    var price = document.createElement("p")
+    price.textContent = elem.price;
+    price.setAttribute("class", "prodPrice")
+
+    var name = document.createElement("p")
+    name.textContent = elem.name;
+    name.setAttribute("class", "prodName")
+
+    imgBox.append(img)
+    prodBox.append(price,name)
+    mainBox.append(imgBox,prodBox)
+    document.querySelector("#itemList").append(mainBox)
+});
+
+var totalPrice = bagItems.reduce(function(sum,elem,i,arr){
+    return sum + Number(elem.price)
+},0);
+
+document.querySelector(".displayPrice").textContent = totalPrice;
+
+document.querySelector("#applyCode").addEventListener("click", promoFn)
+function promoFn(){
+    var promo = document.querySelector("#promoCode").value
+    if(promo == "SPECIAL"){
+        var discount = eval(totalPrice * 0.7)
+    }
+    else{
+        alert("Enter valid code")
+    }
+    document.querySelector(".displayPrice").textContent = discount;
+}
